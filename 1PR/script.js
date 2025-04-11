5aedfa90bd43afc2c58806d0fec8f20368886f18
Gabriel Rodrigues <gabrielrdrcb18@gmail.com>
2025-04-11 12:02:23 -0300

    add 1PR

---
+++ 1PR/script.js
@@ -0,0 +1,79 @@
+//GET
+async function getAllData() {
+    const url = "http://localhost:8080/o/headless-delivery/v1.0/sites/20117/blog-postings/"
+      
+    try {
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+        "Content-Type": "application/json",
+        Authorization:  'Basic ' + btoa('test@liferay.com:learn')
+        } 
+      });
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`)
+      }
+      const data = await response.json();
+      console.log("Data received", data)
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+let id;
+  //POST
+async function postData() {
+    fetch("http://localhost:8080/o/headless-delivery/v1.0/sites/20117/blog-postings", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: 'Basic ' + btoa('test@liferay.com:learn')
+        },
+        body: JSON.stringify({
+            articleBody: "This article was posted by Gabriel.R via REST services provided by Liferay DXP.",
+            headline: "Test Blog Entry from REST Services"
+        })
+    })
+
+        .then(res => {
+            if(!res.ok) {
+                return res.text().then(texto => {
+                    throw new Error(`Error ${res.status}: ${texto}`);
+                });
+            }
+            return res.json();
+        }) 
+        .then(data => {console.log("Post sucessfuly created", data);
+        id = data.id;
+        getSingleData(id);
+        })
+        .catch(err => console.error("Erro:", err.message))
+}
+//GETSINGLE
+
+async function getSingleData(id) {
+    console.log("ID recebido:", id)
+
+    try {
+        const res = await fetch(`http://localhost:8080/o/headless-delivery/v1.0/blog-postings/${id}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: 'Basic ' + btoa('test@liferay.com:learn')
+            }
+    });
+        if (!res.ok) {
+            const texto = await res.text();
+            throw new Error(`Erro ${res.status}: ${texto}`);
+        }
+
+        const data = await res.json();
+    } catch (err) {
+        console.error("Erro:", err.menssage)
+    }
+}
+
+//DELETE
+async function deletePost(id) {
+    fetch()
+}
