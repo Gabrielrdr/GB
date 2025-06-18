7d0c0c1be88984fb5bde53d010b460ce2a7e7e72
Gabriel Rodrigues <gabrielrdrcb18@gmail.com>
2025-06-18 08:39:45 -0300

    chess and index.html

---
+++ java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    final int x = 5;
+    int y = 2;
+
+     public static void ad() {
+     Main obj = new Main();
+     System.out.println(obj.x + obj.y);
+     System.out.println("Hello");
+    }
+
+    public static void main (String[] args) {
+     Main myObj = new Main();
+     System.out.println(myObj.x);
+
+    }
+}
